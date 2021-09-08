436c6b85d574cf11ec273b326c519505b187a754
hw269 <hw269@duke.edu>
2021-09-08 10:14:39 -0400

    -10 to 4th cannot be right

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -11,12 +11,11 @@ unsigned int power_helper(unsigned x, unsigned y, unsigned ans) {
   }
   return (power_helper(x, y - 1, ans * x));
 }
-/*
+
 int main(void) {
   printf("3 to the pow of 4 is %u\n", power(3, 4));
-  printf("0 to the pow of 0 is %u\n", power(0, 0));
+  printf("-10 to the pow of 4 is %u\n", power(0, 0));
   printf("-1 to the pow of 3 is %d\n", power(-1, 3));
   printf("7 to the pow of 2 is %u\n", power(7, 2));
   return EXIT_SUCCESS;
 }
-*/
