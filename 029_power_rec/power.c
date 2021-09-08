383175d539d46e2dd4d598403dd8c108e8928f75
hw269 <hw269@duke.edu>
2021-09-07 22:10:12 -0400

    !

--- 029_power_rec/power.c
+++ 029_power_rec/power.c
@@ -11,7 +11,7 @@ unsigned int power_helper(unsigned x, unsigned y, unsigned ans) {
   }
   return (power_helper(x, y - 1, ans * x));
 }
-
+/*
 int main(void) {
   printf("3 to the pow of 4 is %u\n", power(3, 4));
   printf("0 to the pow of 0 is %u\n", power(0, 0));
@@ -19,3 +19,4 @@ int main(void) {
   printf("7 to the pow of 2 is %u\n", power(7, 2));
   return EXIT_SUCCESS;
 }
+*/
