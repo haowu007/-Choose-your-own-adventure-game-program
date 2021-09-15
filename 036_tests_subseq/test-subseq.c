194d0e02133b4d15d80d3221a8bb6d43e29d56cb
hw269 <hw269@duke.edu>
2021-09-15 09:46:32 -0400

    !

---
+++ 036_tests_subseq/test-subseq.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+size_t maxSeq(int * array, size_t n);
+
+void run_check(int * a, size_t n, size_t ans) {
+  if (maxSeq(a, n) != ans) {
+    exit(EXIT_FAILURE);
+  }
+}
+
+int main(void) {
+  //  ans = maxSeq(NULL, 0);
+
+  // ans = maxSeq(NULL, 3);
+  // ans = maxSeq(NULL, 1);
+  //  char chararry[] = {'a', 'b'};
+  // ans = maxSeq(chararry, 2);
+  // none-valid test cases for the first parameter done
+  run_check(NULL, 0, 0);
+  int arr01[] = {1};
+  run_check(arr01, 1, 1);
+  int arr1[] = {1, 2, 3, 4, 5};
+  run_check(arr1, 5, 5);
+  int arr2[] = {3, 3, 3, 3, 3};
+  run_check(arr2, 5, 1);  //all-the-same array
+  int arr3[] = {5, 4, 3, 2, 1};
+  run_check(arr3, 5, 1);                           //no-increasing array
+  int arr4[] = {-1, -2, -1, 0, 1, 2, 3, 2, 1, 1};  //ordinary array
+  run_check(arr4, 10, 6);
+  int arr5[] = {0, 1, 2, 2, 3, 4, 5, 4, 3, 2, 1};
+  run_check(arr5, 11, 4);
+  int arr6[] = {1, 2, 2, 3, 4, 5};
+  run_check(arr6, 6, 4);
+  int arr7[] = {1, 2, 3, 4, 5, 4, 3, 2, 3, 4};
+  run_check(arr7, 10, 5);
+  return EXIT_SUCCESS;
+}
