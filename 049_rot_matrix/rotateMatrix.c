daa5c61d3d4da9afc2fc053dfcea90527f99b2fe
hw269 <hw269@duke.edu>
2021-09-23 19:18:51 -0400

    ll

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -12,18 +12,19 @@ int main(int argc, char ** argv) {
   if (input_matrix == NULL) {
     perror("wrong input!fopen failed!");
     fprintf(stderr, "while trying to open %s", argv[1]);
+    exit(EXIT_FAILURE);
   }
   int i = 0;
   int j = 9;  // start from ans[0][9] to ans[10][9]
   int cur = 0;
   char cur_char = 'a';  //just trying to give it a initial value
-  //int count = 0;
+  int count = 0;
   for (j = 9; j >= 0; j--) {
     for (i = 0; i <= 10; i++) {
       if ((cur = fgetc(input_matrix)) != EOF) {
         cur_char = (char)cur;
         if (i == 10 && cur_char != '\n') {
-          fprintf(stderr, "no new line character found at the end of row!");
+          fprintf(stderr, "no new line character found at the end of row %d !", j);
           exit(EXIT_FAILURE);
         }
         if (cur_char == '\n' && i != 10) {
@@ -31,14 +32,21 @@ int main(int argc, char ** argv) {
           exit(EXIT_FAILURE);
         }
         ans[i][j] = cur_char;
-        //    count++;
+        count++;
       }
     }
+  }
+
+  if (count != 110) {
+    //perror("too few elements!");
+    fprintf(stderr, "too few elements for %s, only %d elements found \n", argv[1], count);
+    exit(EXIT_FAILURE);
   }
   // printf("%d elements added\n", count);
   if (fclose(input_matrix) != 0) {
     perror("fclose failed!");
     fprintf(stderr, "while tring to close %s\n", argv[1]);
+    exit(EXIT_FAILURE);
   }
   //now is time to print out
   for (i = 0; i <= 9; i++) {
