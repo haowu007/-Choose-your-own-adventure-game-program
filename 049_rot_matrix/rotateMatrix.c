5457453f2cadaa2b6a0ca6d410942ad90cb41179
hw269 <hw269@duke.edu>
2021-09-23 18:45:23 -0400

    !

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -9,11 +9,15 @@ int main(int argc, char ** argv) {
   char ans[11][10];
 
   FILE * input_matrix = fopen(argv[1], "r");
+  if (input_matrix == NULL) {
+    perror("wrong input!fopen failed!");
+    fprintf(stderr, "while trying to open %s", argv[1]);
+  }
   int i = 0;
   int j = 9;  // start from ans[0][9] to ans[10][9]
   int cur = 0;
-  char cur_char;
-  int count = 0;  //
+  char cur_char = 'a';  //just trying to give it a initial value
+  //int count = 0;
   for (j = 9; j >= 0; j--) {
     for (i = 0; i <= 10; i++) {
       if ((cur = fgetc(input_matrix)) != EOF) {
@@ -27,10 +31,11 @@ int main(int argc, char ** argv) {
           exit(EXIT_FAILURE);
         }
         ans[i][j] = cur_char;
-        count++;
+        //    count++;
       }
     }
   }
+  // printf("%d elements added\n", count);
   if (fclose(input_matrix) != 0) {
     perror("fclose failed!");
     fprintf(stderr, "while tring to close %s\n", argv[1]);
@@ -42,5 +47,6 @@ int main(int argc, char ** argv) {
     }
     printf("\n");
   }
+  //  printf("BTW,the last element was: %d", (int)ans[10][0]);
   return EXIT_SUCCESS;
 }
