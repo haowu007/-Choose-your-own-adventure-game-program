038b42c01f51e1b07934c9b52e7e420a2e9573b7
hw269 <hw269@duke.edu>
2021-09-22 21:03:06 -0400

    !

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -8,7 +8,9 @@ int break_function(FILE * f) {
   for (int i = 0; i <= 25; i++) {
     count_array[i] = 0;
   }
+  int count;
   while ((c = fgetc(f)) != EOF) {
+    count++;
     //    printf("we are in the while! c is %c!\n", c);
     if (isalpha(c)) {
       // printf("we are in the if!\n");
@@ -26,10 +28,10 @@ int break_function(FILE * f) {
       max_label = i;
     }
   }
-  if (max_label == -1) {
-    return 0;
+  if (count == 0) {
+    return -1;
   }
-  return (max_label >= 4) ? (max_label - 4) : max_label + 21;
+  return (max_label >= 4) ? (max_label - 4) : max_label + 22;
 }
 
 int main(int argc, char ** argv) {
@@ -48,6 +50,8 @@ int main(int argc, char ** argv) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
   }
-  printf("%d\n", ans);
+  if (ans != -1) {
+    printf("%d\n", ans);
+  }
   return EXIT_SUCCESS;
 }
