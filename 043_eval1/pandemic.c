3ea9f0d8ee7c84f4e27fcf53f8e9f55309fc772f
hw269 <hw269@duke.edu>
2021-09-24 15:28:14 -0400

    !

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -40,6 +40,35 @@ country_t parseLine(char * line) {
 
 void calcRunningAvg(unsigned * data, size_t n_days, double * avg) {
   //WRITE ME
+  if (data == NULL) {
+    perror("data not found!\n");
+    fprintf(stderr, "filed when trying to find data.\n");
+    exit(EXIT_FAILURE);
+  }
+  if (avg == NULL) {
+    perror("no where to store average!\n");
+    fprintf(stderr, "failed when tring to find average array\n");
+    exit(EXIT_FAILURE);
+  }
+  if (n_days < 7) {
+    perror("to few days!\n");
+    fprintf(stderr, "No enough days to calculate!\n");
+    exit(EXIT_FAILURE);
+  }
+  unsigned * cur_start_day = data;
+  unsigned cur_total = 0;
+  for (size_t i = 0; i < n_days - 6; i++) {
+    cur_total = 0;
+    for (int j = 0; j < 7; j++) {
+      //printf("The %d th data in this cycle is:%d \n", j, *cur_start_day);
+      cur_total += *cur_start_day;
+      cur_start_day++;
+    }
+    *avg = ((double)cur_total) / 7;
+    avg++;
+    data++;
+    cur_start_day = data;
+  }
 }
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum) {
