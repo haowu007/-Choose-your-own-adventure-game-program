972b88aec02b9d125d5462a8bed8f19558608221
hw269 <hw269@duke.edu>
2021-09-25 13:37:11 -0400

    wo

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -143,8 +143,7 @@ void printCountryWithMax(country_t * countries,
     }
   }
   if (winners > 1) {
-    fprintf(stderr, "There is a tie between at least two counties");
-    exit(EXIT_FAILURE);
+    printf("There is a tie between at least two countries");
   }
   else {
     printf("%s has the most daily cases with %u\n", ans_country, max_of_all);
