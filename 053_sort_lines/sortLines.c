087ddf02c12e0feebd168e35d4856e130c50478c
hw269 <hw269@duke.edu>
2021-09-27 00:39:06 -0400

    wow

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -9,13 +9,79 @@ int stringOrder(const void * vp1, const void * vp2) {
   const char * const * p2 = vp2;
   return strcmp(*p1, *p2);
 }
+
 //This function will sort data (whose length is count).
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-  //WRITE YOUR CODE HERE!
+void printFromstdin(void) {
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t linecap;
+  size_t i = 0;
+  while (getline(&curr, &linecap, stdin) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    //    printf("the string just read in is:%s\n", lines[i]);
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
 
+void printFromInput(int argc, char ** argv) {
+  char * curr = NULL;
+  char ** lines = NULL;
+  size_t linecap = 0;
+  size_t i = 0;
+  FILE * f = NULL;
+  //  printf("In printFromInput, argc = %d, argv = %s.\n", argc, argv[1]);
+  for (int j = 1; j < argc; j++) {
+    f = fopen(argv[j], "r");
+    if (f == NULL) {
+      fprintf(stderr, "failed while open %s\n", argv[j]);
+      exit(EXIT_SUCCESS);
+    }
+    while (getline(&curr, &linecap, f) >= 0) {
+      lines = realloc(lines, (i + 1) * sizeof(*lines));
+      lines[i] = curr;
+      curr = NULL;
+      i++;
+    }
+    free(curr);
+    curr = NULL;
+    linecap = 0;
+    if (fclose(f) != 0) {
+      fprintf(stderr, "while yring to close %s", argv[j]);
+      exit(EXIT_SUCCESS);
+    }
+    f = NULL;
+  }
+  sortData(lines, i);
+
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+
+  free(lines);
+}
+
+int main(int argc, char ** argv) {
+  if (argc == 1) {  //read from stdin!
+    printFromstdin();
+  }
+
+  if (argc > 1) {
+    //  printf("In main->agrv>1,argc is %d, argv is %s.\n", argc, argv[1]);
+    printFromInput(argc, argv);
+  }
   return EXIT_SUCCESS;
 }
