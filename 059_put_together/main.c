ef5a314b3b4529e3b8e317819b17dc9a718d5d10
hw269 <hw269@duke.edu>
2021-10-06 13:21:01 -0400

    cool

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -37,6 +37,10 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
+  if (argc <= 2) {
+    fprintf(stderr, "Usage: kv.txt, list1.txt ,list2.txt.....\n");
+    return EXIT_FAILURE;
+  }
   //read the key/value pairs from the file named by argv[1] (call the result kv)
   kvarray_t * kv_table = readKVs(argv[1]);
   //count from 2 to argc (call the number you count i)
