ab1bf37ccf94e88afec28eab49138ae533548749
hw269 <hw269@duke.edu>
2021-10-16 14:22:00 -0400

    cool

--- 060_eval2/rand_story.c
+++ 060_eval2/rand_story.c
@@ -1,1 +1,14 @@
 #include "rand_story.h"
+
+char * getoneline(FILE * f) {
+  ssize_t len = 0;
+  size_t sz = 0;
+  char * curr = NULL;
+  if ((len = getline(&curr, &sz, f)) >= 0) {
+    return curr;
+  }
+  else {
+    free(curr);
+    return NULL;
+  }
+}
--- 060_eval2/rand_story.h
+++ 060_eval2/rand_story.h
@@ -1,8 +1,11 @@
 #ifndef __RAND_STORY_H__
 #define __RAND_STORY_H__
 
-#include "provided.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "provided.h"
 //any functions you want your main to use
+char * getoneline(FILE * f);
 
 #endif
