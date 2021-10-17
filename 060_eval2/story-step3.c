f0a17dac077c42855c10d47d294cdc77825e7a30
hw269 <hw269@duke.edu>
2021-10-17 13:23:33 -0400

    o

--- 060_eval2/story-step3.c
+++ 060_eval2/story-step3.c
@@ -1,28 +1,17 @@
 #include "rand_story.h"
 
 int main(int argc, char ** argv) {
-  if (argc != 3 && argc != 4) {  //error handling
+  if (argc != 3) {  //error handling
     fprintf(stderr, "Usage: please type in category/word file and story template file\n");
     exit(EXIT_FAILURE);
   }
   linesandcats_t * LandC = NULL;
   int reuse_enable = 0;
-  if (argc == 4) {  // a '-n' option might be typed in
-    if (strcmp(argv[1], "-n") == 0) {
-      LandC = ParseCategory(argv[2], 0);
-      reuse_enable = 0;
-      parseStoryandchange(argv[3], LandC, reuse_enable);
-    }
-    else {  //error handling
-      fprintf(stderr, "Usage: please tyoe in '-n' to ban words reusing!");
-      exit(EXIT_FAILURE);
-    }
-  }
-  else {
-    LandC = ParseCategory(argv[1], 0);
-    reuse_enable = 1;
-    parseStoryandchange(argv[2], LandC, reuse_enable);
-  }
+
+  LandC = ParseCategory(argv[1], 0);
+  reuse_enable = 1;
+  parseStoryandchange(argv[2], LandC, reuse_enable);
+
   freelinesandcats(LandC);
   return EXIT_SUCCESS;
 }
