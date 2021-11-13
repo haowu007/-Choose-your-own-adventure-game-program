b6c43291b60dec40e17a9c3bf5a7ae494106c7e6
hw269 <hw269@duke.edu>
2021-11-13 18:30:47 -0500

    ppo

--- 102_huff_freq/readFreq.cpp
+++ 102_huff_freq/readFreq.cpp
@@ -1,9 +1,11 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +23,17 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  std::ifstream Myfile(fname, std::ifstream::in);
+  uint64_t * ans = new uint64_t[257];
+  for (size_t i = 0; i < 257; i++) {
+    ans[i] = 0;
+  }
+  char c;
+  while (Myfile.get(c)) {
+    size_t i = (size_t)c;
+    ans[i]++;
+  }
+  ans[256] = 1;
+  Myfile.close();
+  return ans;
 }
