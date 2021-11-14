744b0110eb61ef865011dd677f870407bdbb3e74
hw269 <hw269@duke.edu>
2021-11-13 20:05:11 -0500

    wowow

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t Mypq;
+  for (unsigned i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      Mypq.push(new Node(i, counts[i]));
+    }
+  }
+  while (Mypq.size() > 1) {
+    Node * left = Mypq.top();
+    Mypq.pop();
+    Node * right = Mypq.top();
+    Mypq.pop();
+    Mypq.push(new Node(left, right));
+  }
+
+  return Mypq.top();
+}
