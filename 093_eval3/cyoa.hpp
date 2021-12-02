7e56ac6a4eeec26d28a24b927644d0fab7e5a490
hw269 <hw269@duke.edu>
2021-12-02 15:55:40 -0500

    eee

--- 093_eval3/cyoa.hpp
+++ 093_eval3/cyoa.hpp
@@ -7,6 +7,7 @@
 #include <stack>     //for std::stack
 #include <string>    //for std::string
 #include <vector>    //for std::vector
+/***This class stores the information we read in from a given pageX.txt file ***/
 class Page {
  public:
   std::vector<std::string>
@@ -19,24 +20,36 @@ class Page {
   void print_page();  //print out the content of a page in the format that step1 asked
 };
 
+/***This class stores the information that is of further use in step2/3/4 ***/
 class Useful_infor {
  public:
   std::map<size_t, std::vector<size_t> >
       adj_map;  //adjacent map ,key = page number, value = page numbers of its neighbours
   std::set<size_t> win_pages_set;  // the page number of all the "WIN" pages
   size_t page_num;
-  size_t max_choice_page;
+  size_t max_choice_page;  //The maximum page number that appeared in a 'choice' section
   Useful_infor() {
     page_num = 0;
     max_choice_page = 0;
   }
 };
 
-bool isPositiveNum(std::string & s);
-bool parseOneChoice(Page & page, std::string & cur, Useful_infor & UI);
-int ParsePage(Page & page, const char * filename, Useful_infor & UI);
-int examine_whole_story(char * directory, Useful_infor & UI);
-void ReadaStory(char * directory_name, size_t num_page);
-void printStoryDepth(char * directory);
-void printPath(std::vector<std::pair<size_t, size_t> > & vec);
-void FindAllWin(char * directory);
+bool isPositiveNum(std::string & s);  //returns true if s reprensents a positive integer
+bool parseOneChoice(
+    Page & page,
+    std::string & cur,
+    Useful_infor & UI);  //parse one single line corresponding to one choice
+int ParsePage(Page & page,
+              const char * filename,
+              Useful_infor & UI);  //parse a whole page according to the required format
+int examine_whole_story(
+    char * directory,
+    Useful_infor & UI);  //check the format for a directory containing a story
+void ReadaStory(
+    char * directory_name,
+    size_t num_page);  //let user play the story by selecting different chioces
+void printStoryDepth(
+    char * directory);  //calculate can print the 'story depth' as required in cyoa-step3
+void printPath(std::vector<std::pair<size_t, size_t> > & vec);  //print one single path
+void FindAllWin(
+    char * directory);  //find and print out all paths to the 'WIN' page(s) in a story
